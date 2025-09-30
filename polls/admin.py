15da83c60820b7e29453d9b6c6a07164430ce238
unknown <sampurna789@gmail.com>
2025-09-30 10:56:18 -0400

    Django Polls app with AWS EB deployment

---
+++ polls/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date")
+    inlines = [ChoiceInline]
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("choice_text", "question", "votes")
